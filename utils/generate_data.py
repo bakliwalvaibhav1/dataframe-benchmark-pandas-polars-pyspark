c0862290c0086cf4d5c5c6123a63d3dcfa533e45
bakliwalvaibhav1 <vbakliwalcodes@gmail.com>
2025-04-15 19:05:24 -0500

    fix: modified timestamp to overlap

--- utils/generate_data.py
+++ utils/generate_data.py
@@ -2,7 +2,7 @@ import random
 import pandas as pd
 from datetime import datetime, timedelta
 
-ROW_COUNT = 100000
+ROW_COUNT = 10_000_000_000
 START_DATE = datetime(2020, 1, 1)
 CATEGORIES = ['A', 'B', 'C']
 
@@ -14,7 +14,7 @@ for i in range(ROW_COUNT):
         "category": random.choice(CATEGORIES),
         "value1": round(random.uniform(3, 9), 2),
         "value2": random.randint(20, 60),
-        "timestamp": (START_DATE + timedelta(days=i)).strftime("%Y-%m-%d")
+        "timestamp": (START_DATE + timedelta(days=i % 3650)).strftime("%Y-%m-%d")
     }
     rows.append(row)
 
